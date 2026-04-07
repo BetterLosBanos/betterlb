1802669b0830ea73cc38b02cf86200ab670f30c6
miconficker <mbx@pm.me>
2026-04-08 01:06:18 +0800

    chore: delete deprecated submit-contribution API endpoint

--- functions/api/submit-contribution.ts
+++
@@ -1,9 +0,0 @@
-export const onRequestPost: PagesFunction = async () => {
-  return Response.json(
-    {
-      error: 'This endpoint is deprecated. Please use the contribute form.',
-      contribute_url: 'https://betterlb.org/contribute',
-    },
-    { status: 410 }
-  );
-};
